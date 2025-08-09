771876343a7d5917e97243b82ee087d3600e5fc4
seohyun <easeohyun@gmail.com>
2025-08-10 02:32:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
-const CACHE_VERSION = 2; 
+const CACHE_VERSION = 3;
 const CURRENT_CACHE_NAME = `umamusume-filter-cache-v${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/workers/filterWorker.js',
-  '/characters.json',
-  'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap',
+  './index.html',
+  './style.css',
+  './script.js',
+  './workers/filterWorker.js',
+  './characters.json',
+  './skill-descriptions.json',
+  'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@400;500;700&display=swap',
   'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
 ];
 
@@ -15,7 +16,6 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CURRENT_CACHE_NAME)
     .then((cache) => {
-      console.log('Opened cache');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
@@ -27,7 +27,6 @@ self.addEventListener('activate', (event) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CURRENT_CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -37,27 +36,10 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  const { request } = event;
-  const url = new URL(request.url);
-  if (url.pathname.endsWith('/characters.json')) {
-    event.respondWith(
-      caches.open(CURRENT_CACHE_NAME).then(cache => {
-        return cache.match(request).then(cachedResponse => {
-          const fetchPromise = fetch(request).then(networkResponse => {
-            cache.put(request, networkResponse.clone());
-            return networkResponse;
-          });
-          return cachedResponse || fetchPromise;
-        });
-      })
-    );
-  } 
-  else if (ASSETS_TO_CACHE.some(asset => url.pathname.endsWith(asset))) {
-    event.respondWith(
-      caches.match(request).then(response => {
-        return response || fetch(request);
-      })
-    );
-  }
+  event.respondWith(
+    caches.match(event.request)
+    .then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
-
